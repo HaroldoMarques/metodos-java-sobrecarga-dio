418b7260d0180d9feede764a660b7c6d72c976c7
Haroldo <haroldocontrole@gmail.com>
2022-10-20 10:33:18 -0300

    first commit

---
+++ src/br/com/sobrecarga/metodosjava/intellij/dio/Execucao.java
@@ -0,0 +1,15 @@
+package br.com.sobrecarga.metodosjava.intellij.dio;
+
+public class Execucao {
+
+    public static void main(String[] args) {
+
+        // Quadriláteros
+        System.out.println("Exercícios Quadriláteros");
+        Quadrilateros.area(3);
+        Quadrilateros.area(5d,5d);
+        Quadrilateros.area(7,8,9);
+        Quadrilateros.area(5f,5f);
+
+    }
+}
